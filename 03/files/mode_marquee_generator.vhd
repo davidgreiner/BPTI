e6d3a46014973f40670e4a5eaedeed435bd11ab5
Steffen Peikert <steffen.peikert@accuraform.com>
2015-05-21 18:38:19 +0200

    renamed architecture to _generator

--- 03/files/mode_marquee_generator.vhd
+++ 03/files/mode_marquee_generator.vhd
@@ -10,10 +10,10 @@ entity entity_marquee_generator is
     );
 end entity entity_marquee_generator;
 
-architecture architecture_marquee_formula of entity_marquee_generator is
+architecture architecture_marquee_generator of entity_marquee_generator is
 begin
   marquee_formula_p : process(val_in)
   begin
     val_out <= (7 sra val_in) + ((val_in / 6) sra (val_in mod 6)) + (val_in / 7);
   end process marquee_formula_p;
-end architecture_marquee_formula;
+end architecture_marquee_generator;
