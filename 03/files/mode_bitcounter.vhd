d9398a85de098e9e31ac17fb4a4168d8a0178fce
David Greiner <david@ultitech.de>
2015-05-21 17:10:13 +0200

    fixed bit counter limit

--- 03/files/mode_bitcounter.vhd
+++ 03/files/mode_bitcounter.vhd
@@ -16,5 +16,5 @@ architecture architecture_bitcounter of entity_bitcounter is
     port (cnt_in : in std_logic; cnt_mode : in std_logic; cnt_rst : in std_logic; cnt_limit : in integer; cnt_out : out std_logic);
   end component;
 begin
-  counter_pm : entity_counter port map (cnt_in => bit_in; cnt_mode => bit_mode; cnt_rst => bit_rst; cnt_limit => 8; cnt_out => bit_out);
+  counter_pm : entity_counter port map (cnt_in => bit_in; cnt_mode => bit_mode; cnt_rst => bit_rst; cnt_limit => 255; cnt_out => bit_out);
 end architecture_bitcounter;
